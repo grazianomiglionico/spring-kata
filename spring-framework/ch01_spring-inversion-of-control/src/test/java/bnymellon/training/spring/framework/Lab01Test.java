bf8fb46278d8718ebb469eefc697e604ab5143a1
Massimo Ugues <massimo.ugues@alten.it>
2023-02-09 09:15:33 +0100

    Implemented related parts for tests for chapter 1

--- spring-framework/ch01_spring-inversion-of-control/src/test/java/bnymellon/training/spring/framework/Lab01Test.java
+++ spring-framework/ch01_spring-inversion-of-control/src/test/java/bnymellon/training/spring/framework/Lab01Test.java
@@ -51,17 +51,12 @@ public class Lab01Test {
     @Order(1)
     public void instantiateThroughConstructor() {
 
-        // TODO: Add a bean definition with id=vehicleThroughConstructor in the
-        //  src/test/resources/test-instantiation-spring-config.xml
-        // UNCOMMENT BELOW CODE AND FIX IT.
-/*
         Vehicle vehicle = (Vehicle) context.getBean("vehicleThroughConstructor");
         assertEquals(
                 "2017 car",
                 vehicle.toString(),
                 "The vehicle should be 2017 car"
         );
-*/
 
     }
 
@@ -70,19 +65,12 @@ public class Lab01Test {
     @Order(2)
     public void instantiateThroughStaticFactory() {
 
-        // TODO: Add a bean definition with id=vehicleThroughStaticFactory in the
-        //  src/test/resources/test-instantiation-spring-config.xml
-        // TODO: Fill out the static method to the VehicleFactory class to return
-        //  a Vehicle with year = 2016 and type = truck.
-        // UNCOMMENT BELOW CODE AND FIX IT.
-/*
         Vehicle vehicle = (Vehicle) context.getBean("vehicleThroughStaticFactory");
         assertEquals(
                 "2016 truck",
                 vehicle.toString(),
                 "The vehicle should be 2016 truck"
         );
-*/
     }
 
     @Test
@@ -90,20 +78,11 @@ public class Lab01Test {
     @Order(3)
     public void instantiateThroughInstanceFactory() {
 
-        // TODO: Add a bean definition with id=vehicleFactory in the
-        //  src/test/resources/test-instantiation-spring-config.xml
-        // TODO: Add a bean definition with id=vehicleThroughInstanceFactory in the
-        //  src/test/resources/test-instantiation-spring-config.xml
-        // TODO: Fill out the instance method to the VehicleFactory class to return
-        //  a Vehicle with year = 2015 and type = bike.
-        // UNCOMMENT BELOW CODE AND FIX IT.
-/*
         Vehicle vehicle = (Vehicle) context.getBean("vehicleThroughInstanceFactory");
         assertEquals(
                 "2015 bike",
                 vehicle.toString(),
                 "The vehicle should be 2015 bike"
         );
-*/
     }
 }
